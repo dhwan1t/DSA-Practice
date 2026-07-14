360c5665e56fd678bc31f70c6a4ae148b3113f85
DHWANIT VYAS <177022773+dhwan1t@users.noreply.github.com>
2026-07-14 12:04:12 +0530

    Time: 8 ms (19.7%), Space: 99.5 MB (22.78%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,58 +9,20 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head){
-        // if(head == null || head.next == null){
-        //     return head;
-        // }
-        // ListNode revNode = reverse(head.next);
-        // head.next.next = head;
-        // head.next = null;
-        // return revNode;
-        ListNode curr = head;
-        ListNode agla = head;
-        ListNode prev = null;
-
-        while(curr != null){
-            agla = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = agla;
-        }
-        return prev;
-    }
-
-    public ListNode middle(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
-            return true;
+        List<Integer> list = new ArrayList<>();
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
         }
 
-        ListNode midOfNode = middle(head);
-        ListNode left = head;
-        ListNode right = midOfNode.next;
-        midOfNode.next = null;
+        int left = 0;
+        int right = list.size()-1;
 
-        ListNode revright = reverse(right);
-        ListNode rhead = revright;
-        ListNode lhead = left;
-
-        while(lhead != null && rhead != null){
-            if(lhead.val != rhead.val){
-                return false;
-            }
-            lhead = lhead.next;
-            rhead = rhead.next;
+        while(left < right && list.get(left) == list.get(right)){
+            left++;
+            right--;
         }
-        return true;
+        return left >= right;
     }
 }
