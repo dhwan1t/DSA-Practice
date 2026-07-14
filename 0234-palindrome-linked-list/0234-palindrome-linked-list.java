3278f3f0f85a094cbdf5f8cb5721bb4a8c2a85aa
DHWANIT VYAS <177022773+dhwan1t@users.noreply.github.com>
2026-07-14 12:38:47 +0530

    Time: 7 ms (29.87%), Space: 94.5 MB (55.75%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,20 +9,45 @@
  * }
  */
 class Solution {
+    public ListNode rev(ListNode head){
+        ListNode curr = head;
+        ListNode aage = head;
+        ListNode prev = null;
+
+        while(curr != null){
+            aage = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = aage;
+        }
+        return prev;
+    }
+    public ListNode mid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        while(head != null){
-            list.add(head.val);
-            head = head.next;
-        }
+        if(head == null || head.next == null) return true;
 
-        int left = 0;
-        int right = list.size()-1;
+        ListNode midNode = mid(head);
+        ListNode left = head;
+        ListNode right = midNode.next;
+        midNode.next = null;
 
-        while(left < right && list.get(left) == list.get(right)){
-            left++;
-            right--;
+        ListNode rev = rev(right);
+        ListNode rhead =  rev;
+        ListNode lhead = left;
+
+        while(lhead != null && rhead != null){
+            if(lhead.val != rhead.val) return false;
+            lhead = lhead.next;
+            rhead = rhead.next;
         }
-        return left >= right;
+        return true;
     }
 }
