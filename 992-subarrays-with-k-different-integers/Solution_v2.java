78a51b0afb086862d9485d2b75a096ebc4df0b73
Dhwanit <177022773+dhwan1t@users.noreply.github.com>
2026-07-31 18:32:04 +0530

    LeetSync: add solution for subarrays-with-k-different-integers (v2)

---
+++ 992-subarrays-with-k-different-integers/Solution_v2.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int helper(int[] nums, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        int i = 0;
+        int j = 0; 
+        while(j < nums.length){
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            while(i < j && map.size() > k){
+                map.put(nums[i], map.get(nums[i]) - 1);
+                if(map.get(nums[i]) == 0)
+                    map.remove(nums[i]);
+                i++; 
+            }
+            ans += j - i + 1;
+            j++;
+        }
+        return ans;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        if(k == 1) return helper(nums, k);
+        System.out.println(helper(nums, k) + " " + helper(nums, k-1));
+        return helper(nums, k) - helper(nums, k - 1);
+    }
+}
