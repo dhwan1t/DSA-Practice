369fc04992f221792f46b6316eb7cae40957360e
Dhwanit <177022773+dhwan1t@users.noreply.github.com>
2026-08-03 18:45:18 +0530

    LeetSync: add solution for count-of-unfinished-tasks-after-each-shift (v2)

---
+++ 4010-count-of-unfinished-tasks-after-each-shift/Solution_v2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] countTasks(int[] tasks, int[] shifts) {
+        long[] prefix = new long[tasks.length+1];
+        
+        for(int i = 0; i<tasks.length; i++){
+            prefix[i+1] = tasks[i]+prefix[i];
+        }
+        long total = prefix[prefix.length-1];
+        
+        long work = 0;
+        int[] res = new int[shifts.length];
+        
+        for(int i = 0; i < shifts.length; i++){
+            work += shifts[i];
+            
+            if(work >= total) {
+                res[i] = 0;
+                work = 0;
+            }
+            
+            else{
+                int l = 0, h = tasks.length;
+                int store = 0;
+                while(l <= h){
+                    int mid = l+(h-l)/2;
+                    if(prefix[mid] <= work){
+                        l = mid+1;
+                        store = mid;
+                    }
+                    else{
+                        h = mid - 1;
+                    }
+                }
+                res[i] = tasks.length-store;
+            }
+        }
+        return res;
+        
+    }
+}
