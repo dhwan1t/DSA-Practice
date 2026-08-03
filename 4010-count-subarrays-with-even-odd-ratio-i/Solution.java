b03b1b667c21879cc17ba8ff4695c1b5df2ca8fa
Dhwanit <177022773+dhwan1t@users.noreply.github.com>
2026-08-03 18:12:56 +0530

    LeetSync: add solution for count-subarrays-with-even-odd-ratio-i (v1)

---
+++ 4010-count-subarrays-with-even-odd-ratio-i/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countRatioSubarrays(int[] nums, int a, int b) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            int x =0; 
+            int y = 0;
+            for(int j = i; j < nums.length; j++){
+                if(nums[j] % 2 == 0) x++;
+                else y++;
+                if(x * b <= y * a){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
