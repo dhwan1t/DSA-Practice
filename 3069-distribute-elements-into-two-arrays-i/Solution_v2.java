af51bba1ca3dfd34bf65f5e3618d3abb6ad4ab32
Dhwanit <177022773+dhwan1t@users.noreply.github.com>
2026-08-19 13:59:32 +0530

    LeetSync: add solution for distribute-elements-into-two-arrays-i (v2)

---
+++ 3069-distribute-elements-into-two-arrays-i/Solution_v2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        l1.add(nums[0]);
+        l2.add(nums[1]);
+
+        for(int i=2; i<nums.length; i++){
+            if(l1.get(l1.size()-1) > l2.get(l2.size()-1)){
+                l1.add(nums[i]);
+            }
+            else{
+                l2.add(nums[i]);
+            }
+        }
+
+        int[] out = new int[nums.length];
+        int idx = 0;
+        for(int i: l1){
+            out[idx++] = i;
+        }
+        for(int i: l2){
+            out[idx++] = i;
+        }
+        return out;
+    }
+}
